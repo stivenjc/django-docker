621870f2d8ae0c4137039bdfa6d826dd53c01c4d
stiven <adisjimenez@misena.edu.co>
2023-06-28 09:27:43 -0500

    correcion de nombre de ruta

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('docss', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path("api/", include("config.api_router", namespace='api')),
