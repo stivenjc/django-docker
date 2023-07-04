7f87086720a439bfb087376727f416b1bb23da60
stiven <adisjimenez@misena.edu.co>
2023-07-04 15:25:56 -0500

    test deploy :) 2 database

--- config/settings/production.py
+++ config/settings/production.py
@@ -24,12 +24,8 @@ LOGGING = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
