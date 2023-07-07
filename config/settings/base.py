31c171a0e03f05fda604fde7483200cd5a15ff50
stiven <adisjimenez@misena.edu.co>
2023-07-07 10:39:21 -0500

    redis implementado

--- config/settings/base.py
+++ config/settings/base.py
@@ -87,6 +87,19 @@ REST_FRAMEWORK = {
     ]
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': config('URL_REDIS'),  # Utiliza el nombre del servicio definido en el docker-compose.yml
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
