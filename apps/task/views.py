bb6add2b987db992fda77db57868fa79a1ea2b8a
stiven <adisjimenez@misena.edu.co>
2023-07-07 16:05:36 -0500

    all for today

--- apps/task/views.py
+++ apps/task/views.py
@@ -10,17 +10,3 @@ class TaskModelViewSet(ModelViewSet):
     serializer_class = TaskSerializers
     permission_classes = [IsAuthenticated]
     queryset = Task.objects.all()
-
-
-    def list(self, request, *args, **kwargs):
-        cached_data = cache.get('data')
-        if cached_data is not None:
-            print('si esta funcionando dato guardado')
-            queryset = cached_data
-            cache.clear()
-        else:
-            print('no, todavia no esta guraddo ya se gaurdara')
-            queryset = Task.objects.all()
-            cache.set('data', queryset, timeout=3600)
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
