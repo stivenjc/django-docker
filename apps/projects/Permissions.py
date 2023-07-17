ea315bd0579293eaefd86958fe9f870ba50955aa
stiven <adisjimenez@misena.edu.co>
2023-07-17 16:23:41 -0500

    mas implementacione

--- apps/projects/Permissions.py
+++ apps/projects/Permissions.py
@@ -2,10 +2,16 @@ from rest_framework.permissions import BasePermission
 
 class RoleBasic(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.groups.filter(name='Intermedio').exists():
+            self.message = 'Debes tener el rol intermdio para poder hacer peticiones.'
+            return False
 
-        # Permitir acceso solo a los usuarios con el rol "Administrador"
-        return request.user.groups.filter(name='Intermedio').exists()
+        return True
 
 class CustomPermission(BasePermission):
     def has_permission(self, request, view):
-        return request.user.has_perm('projects.permision_normal')
+        if not request.user.has_perm('projects.permision_normal'):
+            self.message = 'Lo siento debes tener el permiso requerido para poder hacer peticiones a esta vista'
+            return False
+
+        return True
